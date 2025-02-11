5c5ca0b5aa1b0f479531fc51d9038c0200f72dbd
JagTheFriend <JagTheFriend12@gmail.com>
2025-02-11 20:46:54 +0530

    Setup simple express server
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log("hi");
+import express from "express";
+import { DatType } from "./type";
+
+const app = express();
+const currentData: DatType[] = [];
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.get("/leaderboard", (req, res) => {
+  res.json(currentData);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ src/type.ts
@@ -0,0 +1,1 @@
+export type DatType = { bomb_id: number; level: number; completedTime: number };
